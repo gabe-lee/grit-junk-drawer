7669c8fd9a8ec94a4b89fef32c90ce4be2d6a4d3
Gabriel Anderson <gla.ander@gmail.com>
2023-05-05 19:44:08 -0500

    match_or_return

--- src/lib.rs
+++ src/lib.rs
@@ -1,122 +1,30 @@
-macro_rules! impl_marker_trait {
-    ($TRAIT:tt, $($TYPE:ty),+) => {
-        $(impl $TRAIT for $TYPE {})+
+
+
+//MACRO match_or_return!
+/// Match one or more patterns with an optional guard clause
+/// - If matched, pass the inner expresion back
+/// - Otherwise return the specified return expresion to the enclosing function
+/// 
+/// Similar to [matches!] but instead of resolving to a boolean, it resolves to the inner expression on a match,
+/// or else returns from the enclosing function with the return expresion
+/// ```rust
+/// use grit_junk_drawer::match_or_return;
+/// 
+/// fn main() -> Result<(), String> {
+///     let my_enum = Some(42);
+/// 
+///     let my_val = match_or_return!(my_enum, Some(val) if val < 50, val, Err(String::from("Error")));
+/// 
+///     assert_eq!(my_val, 42);
+///     Ok(())
+/// }
+/// ```
+#[macro_export]
+macro_rules! match_or_return {
+    ($expression:expr, $(|)? $( $pattern:pat_param )|+ $( if $guard: expr )? $(,)?, $inner:expr, $return:expr) => {
+        match $expression {
+            $( $pattern )|+ $( if $guard )? => $inner,
+            _ => return $return
+        }
     };
 }
-
-//#region Numeric Helper Traits
-
-macro_rules! impl_max_trait {
-    ($($TYPE:ty),+) => {
-        $(impl HasMaxValue for $TYPE {
-            #[inline(always)]
-            fn MAX() -> Self {
-                Self::MAX
-            }
-        })+
-    };
-}
-
-macro_rules! impl_min_trait {
-    ($($TYPE:ty),+) => {
-        $(impl HasMinValue for $TYPE {
-            #[inline(always)]
-            fn MIN() -> Self {
-                Self::MIN
-            }
-        })+
-    };
-}
-
-macro_rules! impl_add_sub_zero_traits {
-    ($ONE:expr, $ZERO:expr, $($TYPE:ty),+) => {
-        $(
-            impl CanAddOne for $TYPE {
-                #[inline(always)]
-                fn add_one(self) -> Self {
-                    self + $ONE
-                }
-            }
-
-            impl CanSubOne for $TYPE {
-                #[inline(always)]
-                fn sub_one(self) -> Self {
-                    self - $ONE
-                }
-            }
-
-            impl CanBeZero for $TYPE {
-                #[inline(always)]
-                fn zero() -> Self {
-                    $ZERO
-                }
-            }
-        )+
-    };
-}
-
-pub trait IsInteger {}
-pub trait IsFloat {}
-pub trait IsUnsigned {}
-pub trait IsSigned {}
-
-impl_marker_trait!(IsInteger, u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize);
-impl_marker_trait!(IsUnsigned, u8, u16, u32, u64, u128, usize);
-impl_marker_trait!(IsSigned, i8, i16, i32, i64, i128, isize, f32, f64);
-impl_marker_trait!(IsFloat, f32, f64);
-
-pub trait HasMaxValue {
-    #[allow(non_snake_case)]
-    fn MAX() -> Self;
-}
-
-pub trait HasMinValue {
-    #[allow(non_snake_case)]
-    fn MIN() -> Self;
-}
-
-pub trait CanAddOne {
-    #[allow(non_snake_case)]
-    fn add_one(self) -> Self;
-}
-
-pub trait CanSubOne {
-    #[allow(non_snake_case)]
-    fn sub_one(self) -> Self;
-}
-
-pub trait CanBeZero {
-    #[allow(non_snake_case)]
-    fn zero() -> Self;
-}
-
-impl_max_trait!(u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize, f32, f64);
-impl_min_trait!(u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize, f32, f64);
-impl_add_sub_zero_traits!(1, 0, u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize);
-impl_add_sub_zero_traits!(1.0, 0.0, f32, f64);
-
-const _: () = {
-    fn assert_int<T: IsInteger>() {}
-    fn assert_float<T: IsFloat>() {}
-    fn assert_unsigned<T: IsUnsigned>() {}
-    fn assert_signed<T: IsSigned>() {}
-    fn assert_max<T: HasMaxValue>() {}
-    fn assert_min<T: HasMinValue>() {}
-
-    fn assert_all() {
-        assert_int::<u8>();
-        assert_int::<isize>();
-        assert_float::<f32>();
-        assert_float::<f64>();
-        assert_unsigned::<u8>();
-        assert_unsigned::<usize>();
-        assert_signed::<i8>();
-        assert_signed::<f64>();
-        assert_max::<u8>();
-        assert_max::<f64>();
-        assert_min::<u8>();
-        assert_min::<f64>();
-    }
-};
-
-//#endregion
